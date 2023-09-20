567d999fe3bc0c6993c304a870b23c6c9d282b2a
Martina <martina2019rosario@gmail.com>
2023-09-20 16:31:39 -0300

    libros por genero

--- app.py
+++ app.py
@@ -24,25 +24,26 @@ app.config['MAIL_DEFAULT_SENDER'] = 'konigari2023@gmail.com'
 
 mail = Mail(app)
 
-
 Database.db.init_app(app)
 
-
 #######################CONFIGURACIÓN FLASK MIGRATE################################################
-#Agreo la configuración de Flask Migrate para que podamos realizar las migraciones, y
-#se pueda crear el mapeo de la clase Persona (desarrollada más abajo) hacia la
-#tabla de base de datos que se debe de crear
+# Agreo la configuración de Flask Migrate para que podamos realizar las migraciones, y
+# se pueda crear el mapeo de la clase Persona (desarrollada más abajo) hacia la
+# tabla de base de datos que se debe de crear
 
-#1- Creo un objeto Migrate
+# 1- Creo un objeto Migrate
 migrate = Migrate()
-#2- Inicializo el objeto migrate. Le paso los valores de app, y db
+# 2- Inicializo el objeto migrate. Le paso los valores de app, y db
 migrate.init_app(app, Database.db)
+
+
 #######################CONFIGURACIÓN FLASK MIGRATE################################################
 
 @app.route('/')
 @app.route('/inicio')
 def inicio():
     return redirect(url_for('login'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -52,32 +53,33 @@ def login():
 
         cliente = ClienteLogic.valida_credenciales(nombre_usuario, contraseña)
         if cliente:
-            #Guardo al cliente en la sesión, para que se mueva por la página web sin necesidad
-            #de loguearse a cada momento.
+            # Guardo al cliente en la sesión, para que se mueva por la página web sin necesidad
+            # de loguearse a cada momento.
             session['cliente'] = cliente.to_dict()
-            return render_template('home.html', clienteLogueado = cliente)
+            return render_template('home.html', clienteLogueado=cliente)
         else:
             return render_template('login.html')
     else:
         return render_template('login.html')
 
+
 @app.route('/home')
 def home():
-    #Al cliente que guardé en la sesión en el método login(), lo accedo desde este método, el cual
-    #es para la página principal
+    # Al cliente que guardé en la sesión en el método login(), lo accedo desde este método, el cual
+    # es para la página principal
     cliente_data = session.get('cliente')
     if cliente_data:
         cliente = Cliente.from_dict(cliente_data)
-        return render_template('home.html', clienteLogueado = cliente)
+        return render_template('home.html', clienteLogueado=cliente)
     else:
         return redirect(url_for('login'))
 
 
 @app.route('/enviar_correo')
 def enviar_correo():
-    destinatario = ''
+    destinatario = 'martina2019rosario@gmail.com'
     asunto = 'Prueba de mensaje'
-    mensaje = 'Esto es un mensaje'
+    mensaje = 'Esto es un mensaje para Martu'
 
     msg = Message(asunto, recipients=[destinatario])
     msg.body = mensaje
@@ -88,14 +90,17 @@ def enviar_correo():
     except Exception as e:
         return f'Error al enviar el correo electrónico: {str(e)}'
 
+
 @app.route('/get_all_clientes')
 def get_all_clientes():
     clientes = ClienteLogic.get_all_clientes()
-    return render_template('listado_clientes.html', clientesParam = clientes)
+    return render_template('listado_clientes.html', clientesParam=clientes)
+
+
 @app.route('/eliminar/<int:id>')
 def delete_cliente(id):
     try:
-        #El método delete_cliente(id) de la capa de lógica devuelve un mensaje de éxito
+        # El método delete_cliente(id) de la capa de lógica devuelve un mensaje de éxito
         mensaje = ClienteLogic.delete_cliente(id)
         return redirect(url_for('get_all_clientes'))
     except IntegrityError as e:
@@ -104,6 +109,8 @@ def delete_cliente(id):
         raise e
     except StaleDataError as e:
         raise e
+
+
 @app.route('/agregar_cliente', methods=['GET', 'POST'])
 def add_cliente():
     cliente = Cliente()
@@ -115,19 +122,21 @@ def add_cliente():
             cliente_form.populate_obj(cliente)
             ClienteLogic.add_cliente(cliente)
         return redirect(url_for('get_all_clientes'))
-    return render_template('alta_cliente.html', cliente_agregar = cliente_form)
+    return render_template('alta_cliente.html', cliente_agregar=cliente_form)
+
+
 @app.route('/editar_cliente/<int:id>', methods=['GET', 'POST'])
 def update_cliente(id):
     try:
         cliente = ClienteLogic.get_one_cliente(id)
-        cliente_form = ClienteForm(obj = cliente)
+        cliente_form = ClienteForm(obj=cliente)
         if request.method == 'POST':
             if cliente_form.validate_on_submit():
                 cliente_form.populate_obj(cliente)
                 # El método update_cliente(id) de la capa de lógica devuelve un mensaje de éxito
                 mensaje = ClienteLogic.update_cliente(cliente)
                 return redirect(url_for('get_all_clientes'))
-        return render_template('editar_cliente.html', cliente_editar = cliente_form)
+        return render_template('editar_cliente.html', cliente_editar=cliente_form)
     except NotFound as e:
         raise e
 
@@ -136,6 +145,15 @@ def update_cliente(id):
 def get_libros_by_author(autor):
     libros = LibroAPILogic.get_libros_by_author(autor)
     if libros is not None:
-        return render_template("libros_por_autor.html", librosPorAutor = libros)
+        return render_template("libros_por_autor.html", librosPorAutor=libros)
     else:
         return "No se encontraron los libros"
+
+
+@app.route('/libros/genre/<genero>', methods=['GET'])
+def get_libros_by_genre(genero):
+    libros = LibroAPILogic.get_libros_by_genre(genero)
+    if libros is not None:
+        return render_template("libros_por_genero.html", librosPorGenero=libros)
+    else:
+        return "No se encontraron libros para este género"
